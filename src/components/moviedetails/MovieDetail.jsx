bd1136387671adbe8733fe31f74eebacbd4d7388
BestOlumese <bestolumese@gmail.com>
2024-02-12 15:19:57 +0100

    patch

--- src/components/moviedetails/MovieDetail.jsx
+++ src/components/moviedetails/MovieDetail.jsx
@@ -35,7 +35,7 @@ const MovieDetail = () => {
       <div className="row align-items-center position-relative">
         <div className="col-xl-3 col-lg-4">
           <div className="movie-details-img">
-            <img src={'${apiLink}'+movie.image} alt="" />
+            <img src={apiLink+movie.image} alt="" />
             <a href={movie.youtube_url} className="popup-video"><img src="/img/images/play_icon.png" alt="" /></a>
           </div>
         </div>
